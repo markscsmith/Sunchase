429b18262cdef636d17d3a0aa6b98071f288f289
mscs <mark.s.c.smith@gmail.com>
2024-08-29 00:49:06 -0400

    chore: Add .vscode to .gitignore and create basic application structure

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # Sunchase
 Monitor my solar power and adjust devices based on how much I'm generating / consuming
+# My Solar Monitor
+
+A command-line application to monitor solar production and consumption.
+
+## Installation
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+```bash
+solar-monitor
+```
+# My Solar Monitor
+
+A command-line application to monitor solar production and consumption.
+
+## Installation
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+```bash
+solar-monitor
+```
+# My Solar Monitor
+
+A command-line application to monitor solar production and consumption.
+
+## Installation
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+```bash
+solar-monitor
+```
---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_solar_monitor',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'solar-monitor=my_solar_monitor.main:main',
+        ],
+    },
+)
+from setuptools import setup, find_packages
+
+setup(
+    name='my_solar_monitor',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'solar-monitor=my_solar_monitor.main:main',
+        ],
+    },
+)
+from setuptools import setup, find_packages
+
+setup(
+    name='my_solar_monitor',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'solar-monitor=my_solar_monitor.main:main',
+        ],
+    },
+)
